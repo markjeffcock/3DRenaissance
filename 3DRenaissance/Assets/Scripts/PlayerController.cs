403f2e912ca105e61b69e4d5899c2ee7ca806d10
markjeffcock <mark.jeffcock@gmail.com>
2022-07-12 22:53:54 +0100

    Update PlayerController.cs
    Mismatch of Elements?

--- 3DRenaissance/Assets/Scripts/PlayerController.cs
+++ 3DRenaissance/Assets/Scripts/PlayerController.cs
@@ -7,13 +7,14 @@ public class PlayerController : MonoBehaviour
     public GameObject painting1;
     public GameObject painting2;
 
-    public GameObject[] paintingChange = new GameObject[15];
+    public GameObject[] paintingChange = new GameObject[16];
     // Material[1]
     // Material[2] etc
     // Start is called before the first frame update
+    // Element 0 is the default painting
     void Start()
-    { 
-        painting1.SetActive(true);
+    {
+        paintingChange[0].SetActive(true);
     }
 
     // Update is called once per frame
@@ -21,8 +22,13 @@ public class PlayerController : MonoBehaviour
     {
         
     }
-
-    public void activate1(string empty1)
+    public void activate(int paintingButton)
+    {
+        painting1.SetActive(false);
+        painting2.SetActive(false);
+        paintingChange[paintingButton].SetActive(true);
+    }
+        public void activate1(string empty1)
     {
         painting1.SetActive(true);
         painting2.SetActive(false);
